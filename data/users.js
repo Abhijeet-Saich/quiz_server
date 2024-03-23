f4e79632dc8ff94d27aad63050b47bdf7683dd51
coder_of_cosmos <abhijeetsaich@gmail.com>
2024-03-23 10:35:31 +0530

    credentials updated

--- data/users.js
+++ data/users.js
@@ -4,21 +4,9 @@ const userdata = {
     "users": [
         {
             id: uuid(),
-            username: "prakashsakari",
-            password: "ps12345",
-            emailId: "pk@gmail.com",
-        },
-        {
-            id: uuid(),
-            username: "ashishjangra",
-            password: "aj12345",
-            emailId: "aj@gmail.com",
-        },
-        {
-            id: uuid(),
-            username: "Abhijeet",
-            password: "Matlab@1",
-            emailId: "ac@gmail.com",
+            username: "quiz_user",
+            password: "password",
+            emailId: "xyz@gmail.com",
         },
     ]
 }
